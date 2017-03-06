e1efa251f68ce83c6dfb8c766d939c5e3b436cd3
Michael Swanson <mswanson@1524interactive.com>
2017-03-06 14:28:29 -0600

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -14,11 +14,13 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "secure-shore-81312.herokuapp.com", port: 443]
+  url: [host: "secure-shore-81312.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   # cache_static_manifest: "priv/static/manifest.json"
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
   pool_size: 20
 
 # Do not print debug messages in production
@@ -63,4 +65,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
